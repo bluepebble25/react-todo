05755728d39ecd0a3a2257c700137634a617a84c
bluepebble25 <bluepebble25@gmail.com>
2022-08-02 20:10:30 +0900

    feat: 목록 수정 & 전체삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -20,11 +20,16 @@ function App() {
     setInputValue("");
   };
 
+  const handleDeleteAllClick = () => {
+    setTodoList([]);
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleDeleteAllClick}>Delete All</button>
         </div>
         <Lists todoList={todoList} setTodoList={setTodoList} />
         <Form handleSubmit={handleSubmit} inputValue={inputValue} setInputValue={setInputValue} />
--- src/components/List.js
+++ src/components/List.js
@@ -1,39 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const List = React.memo(({todoList, setTodoList, data, provided, snapshot}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(data.title);
+
   const handleDeleteClick = (id) => {
     let newTodoList = todoList.filter((data) => data.id !== id);
     setTodoList(newTodoList);
   };
 
   const handleCompleteChange = (id) => {
-    let newTodo = todoList.map(data => {
+    let newTodoList = todoList.map(data => {
       if(data.id === id) {
         data.isCompleted = !data.isCompleted;
       }
       return data;
     });
 
-    setTodoList(newTodo);
+    setTodoList(newTodoList);
   };
 
-  return (
-    <div
-      key={data.id}
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${snapshot.isDragging ? "bg-gray-200" : "bg-gray-50"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
-    >
-      <div>
-        <input type="checkbox" defaultChecked={false} className="mr-1" onClick={() => {handleCompleteChange(data.id)}} />
-        <span className={data.isCompleted ? "line-through" : "none"}>{data.title}</span>
+  const handleEditChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    let newTodoList = todoList.map(todo => {
+      if(todo.id === data.id) {
+        data.title = editedTitle;
+      }
+      return todo;
+    });
+
+    setTodoList(newTodoList);
+    setIsEditing(false);
+  };
+
+  if(isEditing) {
+    return (
+      <div
+        className="bg-gray-100 flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded"
+      >
+        <div>
+          <form onSubmit={handleSubmit} >
+            <input
+              className="w-full"
+              value={editedTitle}
+              onChange={handleEditChange}
+            />
+          </form>
+        </div>
+
+        <div>
+          <button
+            className="px-4 py-2"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            save
+          </button>
+          <button
+            className="px-3 py-1 border rounded-full text-gray-500 bg-white"
+            onClick={() => handleDeleteClick(data.id)}
+          >
+            x
+          </button>
+        </div>
       </div>
-      <div>
-        <button className="px-3 py-1 border rounded-full text-gray-500 bg-white" onClick={() => handleDeleteClick(data.id)}>x</button>
+    )
+  } else {
+    return (
+      <div
+        key={data.id}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${snapshot.isDragging ? "bg-gray-300" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+      >
+        <div>
+          <input type="checkbox" defaultChecked={false} className="mr-1" onClick={() => {handleCompleteChange(data.id)}} />
+          <span className={data.isCompleted ? "line-through" : "none"}>{data.title}</span>
+        </div>
+        <div>
+          <button
+            className="px-4 py-2"
+            onClick={() => setIsEditing(true)}
+          >
+            edit
+          </button>
+          <button
+            className="px-3 py-1 border rounded-full text-gray-500 bg-white"
+            onClick={() => handleDeleteClick(data.id)}
+          >
+            x
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  
 });
 
 export default List
